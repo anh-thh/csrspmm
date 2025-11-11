4ba24e1e81a80860dd93102e5fc69f02e8da2dd4
anhthh2207 <tranhuyhoanganh220702@gmail.com>
2025-11-11 17:55:53 -0500

    naive kernel, about the same with torch.sparse

--- include/csr_spmm.cuh
+++ include/csr_spmm.cuh
@@ -38,23 +38,3 @@ __global__ void csr_spmm_naive(
     float* __restrict__ C);
 
 
-__global__ void csr_spmm_naive_2d(
-    int M, int N, int K,
-    float alpha,
-    float beta,
-    const float* __restrict__ A_values,
-    const int* __restrict__ A_col_idx,
-    const int* __restrict__ A_row_ptr,
-    const float* __restrict__ B,
-    float* __restrict__ C);
-
-
-#define WARP_SIZE 32
-__global__ void csr_spmm_warp_per_row(
-    int M, int N, int K,
-    float alpha, float beta,
-    const float* __restrict__ A_values,
-    const int*  __restrict__ A_col_idx,
-    const int*  __restrict__ A_row_ptr,
-    const float* __restrict__ B,
-    float* __restrict__ C);
--- src/csr_spmm.cu
+++ src/csr_spmm.cu
@@ -7,8 +7,6 @@
 Algo parse_csr_algo(const std::string& name) {
     if (name == "cuSPARSELt")   return cuSPARSELt;
     if (name == "naive")        return naive;
-    if (name == "naive_2d")     return naive_2d;
-    if (name == "warp_per_row") return warp_per_row;
 
     std::cerr << "Error: unknown algorithm name '" << name << "'\n";
     std::exit(1);
@@ -35,8 +33,8 @@ void run_csr_spmm(Algo algo,
 
     case naive: {
         const int block_size = 32;
-        const dim3 gridDim(ROUND_UP_TO_NEAREST(M, block_size));
-        const dim3 blockDim(block_size);
+        const dim3 gridDim(ROUND_UP_TO_NEAREST(K, block_size), ROUND_UP_TO_NEAREST(M, block_size));
+        const dim3 blockDim(block_size, block_size);
 
         csr_spmm_naive<<<gridDim, blockDim>>>(M, N, K, 
                                               alpha, beta,
@@ -44,35 +42,6 @@ void run_csr_spmm(Algo algo,
                                               A_col_idx,  
                                               A_row_ptr,
                                               B, C);
-        break;
-    }
-
-    case naive_2d: {
-        const int block_size = 32;
-        const dim3 gridDim(ROUND_UP_TO_NEAREST(K, block_size), ROUND_UP_TO_NEAREST(M, block_size));
-        const dim3 blockDim(block_size, block_size);
-
-        csr_spmm_naive_2d<<<gridDim, blockDim>>>(M, N, K, 
-                                              alpha, beta,
-                                              A_values, 
-                                              A_col_idx,  
-                                              A_row_ptr,
-                                              B, C);
-        break;
-    }
-
-    case warp_per_row: {
-        const int threads_per_block = 128;
-        int warps_per_block   = threads_per_block / WARP_SIZE;
-        int num_blocks        = ROUND_UP_TO_NEAREST(M, warps_per_block);
-        dim3 blockDim(threads_per_block);
-        dim3 gridDim(num_blocks);
-
-        csr_spmm_warp_per_row<<<gridDim, blockDim>>>(M, N, K, alpha, beta,
-                                                     A_values,
-                                                     A_col_idx,
-                                                     A_row_ptr,
-                                                     B, C);
         break;
     }
 
@@ -87,48 +56,12 @@ void run_csr_spmm(Algo algo,
 
 
 
-/**
- * Each thread computes one full row of C (1-D parellelism)
- */
-__global__ void csr_spmm_naive(
-    int M, int N, int K,
-    float alpha, 
-    float beta, 
-    const float* __restrict__ A_values, 
-    const int* __restrict__ A_col_idx, 
-    const int* __restrict__ A_row_ptr, 
-    const float* __restrict__ B, 
-    float* __restrict__ C) 
-{
-    int row = blockIdx.x * blockDim.x + threadIdx.x;
-    if (row >= M) return;
-
-    // this loop handle full row of C
-    #pragma unroll 
-    for (int n = 0; n < K; ++n) {
-        float sum = 0.0f;               // NOTE: consider higher precison for accumulator 
-
-        // non-zero elements in A's row
-        int row_start = A_row_ptr[row];
-        int row_end   = A_row_ptr[row + 1];
-
-        for (int j = row_start; j < row_end; ++j) {
-            int col = A_col_idx[j];
-            float val = A_values[j];
-            // sum += val * B[col * K + n];
-            sum += val * __ldg(&B[col * K + n]);
-        }
-
-        C[row * K + n] = static_cast<float>(alpha * sum + beta * C[row * K + n]);
-    }
-}
-
 
 /**
  * Each thread computes a single element C[row, col], finer-grained parallelism 
  * across both dimensions
  */
-__global__ void csr_spmm_naive_2d(
+__global__ void csr_spmm_naive(
     int M, int N, int K,
     float alpha,
     float beta,
@@ -152,48 +85,11 @@ __global__ void csr_spmm_naive_2d(
         float A_val = A_values[j];
         sum        += A_val * __ldg(&B[A_col * K + col]);
     }
-
-    C[row * K + col] = alpha * sum + beta * C[row * K + col];
+    
+    // C[row * K + col] = alpha * sum + beta * C[row * K + col];
+    C[row * K + col] = alpha * sum + (beta != 0.0f ? beta * C[row * K + col] : 0.0f);
 }
 
-
-__global__ void csr_spmm_warp_per_row(
-    int M, int N, int K,
-    float alpha, float beta,
-    const float* __restrict__ A_values,
-    const int*  __restrict__ A_col_idx,
-    const int*  __restrict__ A_row_ptr,
-    const float* __restrict__ B,
-    float* __restrict__ C)
-{
-    int warp_id = (blockIdx.x * blockDim.x + threadIdx.x) / WARP_SIZE;
-    int lane    = threadIdx.x & (WARP_SIZE - 1);   // threadIdx.x % WARP_SIZE
-
-    if (warp_id >= M) return;
-
-    int row_start = A_row_ptr[warp_id];
-    int row_end   = A_row_ptr[warp_id + 1];
-
-    // iterate over columns of B
-    for (int n = 0; n < K; ++n) {
-        float sum = 0.0f;               // NOTE: consider higher precison for accumulator 
-
-        // each thread processes part of row of A
-        for (int j = row_start + lane; j < row_end; j += WARP_SIZE) {
-            int col = A_col_idx[j];
-            float val = A_values[j];
-            sum += val * __ldg(&B[col * K + n]);
-        }
-
-        // sum all partials in warp using tree reduction 
-        // NOTE: offset >>= 1 = offset / 2
-        for (int offset = WARP_SIZE / 2; offset > 0; offset >>= 1) 
-            sum += __shfl_down_sync(0xffffffff, sum, offset);
-
-        if (lane == 0)
-            C[warp_id * K + n] = alpha * sum + beta * C[warp_id * K + n];
-    }
-}
 
 
 
