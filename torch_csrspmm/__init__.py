e278b30bf4b1b7b1d85e5775d5da62bfa43decb5
anhthh2207 <tranhuyhoanganh220702@gmail.com>
2025-12-07 12:23:52 -0500

    correct interface for warp_per_row_fp4, tested

--- torch_csrspmm/__init__.py
+++ torch_csrspmm/__init__.py
@@ -2,11 +2,14 @@ import torch
 #  from .torch_csrspmm import *
 from ._C import csrspmm_naive as _csrspmm_naive
 from ._C import csrspmm_warp_per_row as _csrspmm_warp_per_row
+from ._C import csrspmm_warp_per_row_fp4 as _csrspmm_warp_per_row_fp4
 
 
 def csrspmm_naive(A_rowptr, A_colidx, A_values, B, alpha, beta):
     return _csrspmm_naive(A_rowptr, A_colidx, A_values, B, alpha, beta)
 
-
 def csrspmm_warp_per_row(A_rowptr, A_colidx, A_values, B, alpha, beta):
     return _csrspmm_warp_per_row(A_rowptr, A_colidx, A_values, B, alpha, beta)
+
+def csrspmm_warp_per_row_fp4(A_rowptr, A_colidx, A_values, B, alpha, beta):
+    return _csrspmm_warp_per_row_fp4(A_rowptr, A_colidx, A_values, B, alpha, beta)
